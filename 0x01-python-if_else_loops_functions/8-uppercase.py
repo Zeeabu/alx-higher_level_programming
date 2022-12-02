e99489d9ac0a32516b83bb42db4a6e22863558f6
Zeeabu <xaynab28@gmail.com>
2022-12-02 02:12:25 -0800

    task 1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 
 uppercase = __import__('8-uppercase').uppercase
 
 uppercase("best")
-uppercase("best school 98 battery street")
+uppercase("Best School 98 Battery street")
