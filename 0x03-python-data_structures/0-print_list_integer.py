3127fdd0d5d08f00722719d25912eec23ca8dc35
Zeeabu <xaynab28@gmail.com>
2022-12-05 10:58:18 -0800

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    print_list_integer = my_list
+    my_list = (10)
+    for i in range(my_list):
+        print(f"These are the integers".format(i))
